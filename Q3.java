87d1f5ec2f917bf566b47dd854a47d9c2b7ed0a4
Electrozap <70527476+Electrozap@users.noreply.github.com>
2020-08-31 20:18:03 +0530

    Create Q3.java

---
+++ Q3.java
@@ -0,0 +1,21 @@
+class Q3
+{
+ void init()
+ {
+  for(int i=0;i<=20;i++)
+  {
+     int ans=(int)(Math.random()*101);
+     System.out.println(ans);
+    }
+     
+     
+    }
+    public static void main()
+    {
+     Q3 obj=new Q3();
+     obj.init();
+        
+    }
+    
+    
+}
